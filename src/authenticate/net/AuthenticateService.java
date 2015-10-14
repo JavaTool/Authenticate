89389fe0140fefcbaa1b3953e0c87a24c7ccdb9c
hyfu <71806228>
2015-10-14 18:15:02 +0800

    md

--- src/authenticate/net/AuthenticateService.java
+++ src/authenticate/net/AuthenticateService.java
@@ -1,63 +1,102 @@
 package authenticate.net;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-import net.io.mina.DispatchPacket;
-import net.io.mina.Packet;
-import net.io.mina.server.DispatchUADecoder;
-import net.io.mina.server.DispatchUAEncoder;
-import net.io.util.IOUtil;
+import org.apache.commons.logging.LogFactory;
 
-import org.apache.mina.common.IoSession;
-import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.transport.socket.nio.SocketAcceptor;
-import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.common.collect.Maps;
+import com.google.common.eventbus.Subscribe;
 
 import authenticate.AuthenticateServer;
 import authenticate.AuthenticateServer.OpcodeInfo;
 import authenticate.account.Account;
+import cg.base.io.SimpleContentHandler;
+import cg.base.io.message.RequestAccountChangePassword;
+import cg.base.io.message.RequestAccountLogin;
+import cg.base.io.message.RequestAccountLogout;
+import cg.base.io.message.RequestAccountRegister;
+import cg.base.io.message.RequestServerRegister;
+import cg.base.io.message.RequestServerUnregister;
+import cg.base.io.message.ResponseAccountChangePassword;
+import cg.base.io.message.ResponseAccountLogin;
+import cg.base.io.message.ResponseAccountLogout;
+import cg.base.io.message.ResponseAccountRegister;
+import cg.base.io.message.ResponseExecuteError;
+import cg.base.log.Log;
+import cg.base.util.SenderUtils;
 import dataplatform.persist.IEntityManager;
+import dataplatform.pubsub.ISimplePubsub;
+import dataplatform.pubsub.impl.SimplePubsub;
+import net.dipatch.ISender;
+import net.io.IMessage;
+import net.io.INetServer;
+import net.io.SimpleContentFactory;
+import net.io.netty.server.NettyTcpServer;
 
 public class AuthenticateService {
 	
-	private static final Logger log = LoggerFactory.getLogger(AuthenticateService.class);
+	private final int port;
 	
-	private static final byte DISPATCH_PACKET_ID = -2;
+	private final Map<String, Account> accounts;
 	
-	private SocketAcceptor acceptor;
+	private final Map<String, String> accountSessions;
 	
-	private String address;
+	private final Map<Integer, OpcodeInfo> methods;
 	
-	private int port;
+	private final Map<String, ISender> senders;
 	
-	private Map<String, IoSession> sessions;
+	private final ISimplePubsub pubsub;
+	
+	private final Log log;
 	
 	private IEntityManager entityManager;
 	
-	private Map<String, Account> accounts;
-	
-	private Map<Integer, OpcodeInfo> methods;
-	
-	public AuthenticateService(String address, int port) {
-		sessions = new ConcurrentHashMap<String, IoSession>();
-		accounts = new ConcurrentHashMap<String, Account>();
-		methods = new HashMap<Integer, OpcodeInfo>();
-		this.address = address;
-		this.port = port;
+	private static class CLog implements Log {
+		
+		private static org.apache.commons.logging.Log log = LogFactory.getLog(Log.class);
+
+		@Override
+		public void info(String info) {
+			log.info(info);
+		}
+
+		@Override
+		public void warning(String warning) {
+			log.warn(warning);
+		}
+
+		@Override
+		public void error(String error) {
+			log.error(error);
+		}
+
+		@Override
+		public void print(String head, String message) {
+			log.info("[" + head + "]" + message);
+		}
+
+		@Override
+		public void error(String error, Throwable t) {
+			log.error(error, t);
+		}
+		
 	}
 	
-	public void bind() throws IOException {
-		acceptor = new SocketAcceptor();
-		SocketAcceptorConfig cfg = new SocketAcceptorConfig();
-		cfg.getFilterChain().addLast("codec", new ProtocolCodecFilter(DispatchUAEncoder.class, DispatchUADecoder.class));
-		acceptor.bind(new InetSocketAddress(address, port),  new AuthenticateHandler(this), cfg);
+	public AuthenticateService(String address, int port) {
+		senders = Maps.newConcurrentMap();
+		accounts = Maps.newConcurrentMap();
+		accountSessions = Maps.newConcurrentMap();
+		methods = Maps.newHashMap();
+		this.port = port;
+		pubsub = new SimplePubsub();
+		log = new CLog();
+	}
+	
+	public void bind() throws Exception {
+		INetServer netServer = new NettyTcpServer(port, new SimpleContentHandler(pubsub), new SimpleContentFactory());
+		netServer.bind();
 		
+		pubsub.subscribe(this);
 		entityManager = AuthenticateServer.getEntityManager();
 
 		for (OpcodeInfo opcode : AuthenticateServer.getOpcodes()) {
@@ -65,18 +104,20 @@ public class AuthenticateService {
 		}
 	}
 	
-	public void registerServer(IoSession session) {
-		String name = getServerName(session);
-		if (!sessions.containsKey(name)) {
-			sessions.put(name, session);
+	@Subscribe
+	public void registerServer(RequestServerRegister requestServerRegister) {
+		String name = getServerName(requestServerRegister);
+		if (!senders.containsKey(name)) {
+			senders.put(name, requestServerRegister.getSender());
 			log.info("registerServer : " + name);
 		}
 	}
-	
-	public void unregisterServer(IoSession session) {
-		String name = getServerName(session);
-		if (sessions.containsKey(name)) {
-			sessions.remove(name);
+
+	@Subscribe
+	public void unregisterServer(RequestServerUnregister requestServerUnregister) {
+		String name = getServerName(requestServerUnregister);
+		if (senders.containsKey(name)) {
+			senders.remove(name);
 			for (Account account : accounts.values()) {
 				if (account.getServerName().equals(name)) {
 					accounts.remove(account.getName());
@@ -85,134 +126,125 @@ public class AuthenticateService {
 			log.info("unregisterServer : " + name);
 		}
 	}
-	
-	public void processPacket(IoSession session, DispatchPacket dp) throws Exception {
-		OpcodeInfo info = methods.get(dp.getPacket().getOpCode());
-		if (info != null) {
-			getClass().getMethod(info.method, IoSession.class, Packet.class, int.class).invoke(this, session, dp.getPacket(), info.returnCode);
-		}
-	}
-	
-	public void registerAccount(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString();
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
+
+	@Subscribe
+	public void registerAccount(RequestAccountRegister requestAccountRegister) {
+//		int serial = packet.getInt();
+		String name = requestAccountRegister.getAccount(), password = requestAccountRegister.getPassword();
+		IMessage message = new ResponseAccountRegister();
 		if (hasAccountName(name)) {
-			pt.put(IOUtil.FALSE);
+			message = new ResponseExecuteError();
 		} else { // OK
 			Account account = new Account();
 			account.setName(name);
 			account.setPassword(password);
 			entityManager.createSync(account);
 
-			pt.put(IOUtil.TRUE);
+			message = new ResponseAccountRegister();
 		}
-		send(session, pt);
+		SenderUtils.send(requestAccountRegister.getSender(), message, log);
 	}
 	
-	private String getServerName(IoSession session) {
-		return session.getRemoteAddress().toString();
+	private String getServerName(IMessage message) {
+		return message.getSender().getIp();
 	}
-	
-	public void accountLogin(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString();
+
+	@Subscribe
+	public void accountLogin(RequestAccountLogin requestAccountLogin) {
+//		int serial = packet.getInt();
+		String name = requestAccountLogin.getAccount(), password = requestAccountLogin.getPassword();
 		Account account = findAccount(name, password);
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
+		IMessage message = new ResponseAccountRegister();
 		if (account == null) {
-			pt.put(IOUtil.FALSE);
-			pt.put(Account.LOGIN_ERROR_NULL);
+			message = new ResponseExecuteError();
 		} else if (accounts.containsKey(name)) {
-			pt.put(IOUtil.FALSE);
-			pt.put(Account.LOGIN_ERROR_REPEAT);
+			message = new ResponseExecuteError();
 		} else {
-			account.setServerName(getServerName(session));
+			account.setServerName(getServerName(requestAccountLogin));
 			accounts.put(name, account);
+			accountSessions.put(requestAccountLogin.getSessionId(), name);
 			
-			pt.put(IOUtil.TRUE);
-			pt.putInt(account.getId());
-			pt.putUTF(name);
-			pt.putUTF(password);
-			pt.putInt(account.getImoney());
+			message = new ResponseAccountLogin();
 		}
-		send(session, pt);
+		SenderUtils.send(requestAccountLogin.getSender(), message, log);
 	}
-	
-	public void deleteAccount(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString();
-		Account account = findAccount(name, password);
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
+
+//	@Subscribe
+//	public void deleteAccount(RequestAccount) {
+//		int serial = packet.getInt();
+//		String name = packet.getString(), password = packet.getString();
+//		Account account = findAccount(name, password);
+//		Packet pt = new Packet(returnCode);
+//		pt.putInt(serial);
+//		if (account == null) {
+//			pt.put(IOUtil.FALSE);
+//		} else {
+//			accounts.remove(name);
+//			entityManager.deleteSync(account);
+//			
+//			pt.put(IOUtil.TRUE);
+//		}
+//		send(session, pt);
+//	}
+
+//	@Subscribe
+//	public void addImoney(IoSession session, Packet packet, int returnCode) {
+//		int serial = packet.getInt();
+//		String name = packet.getString(), password = packet.getString();
+//		int imoney = packet.getInt();
+//		Account account = findAccount(name, password);
+//		Packet pt = new Packet(returnCode);
+//		pt.putInt(serial);
+//		if (account == null) {
+//			pt.put(IOUtil.FALSE);
+//		} else {
+//			int result = account.getImoney() + imoney;
+//			if (result < 0) {
+//				pt.put(IOUtil.FALSE);
+//			} else {
+//				account.setImoney(result);
+//				entityManager.updateSync(account);
+//				
+//				pt.put(IOUtil.TRUE);
+//				pt.putInt(account.getImoney());
+//			}
+//		}
+//		send(session, pt);
+//	}
+
+	@Subscribe
+	public void accountLogout(RequestAccountLogout requestAccountLogout) {
+//		int serial = packet.getInt();
+		String sessionId = requestAccountLogout.getSessionId();
+		String name = accountSessions.get(sessionId);
+		Account account = name == null ? null : accounts.get(name);
+		IMessage message = new ResponseAccountRegister();
 		if (account == null) {
-			pt.put(IOUtil.FALSE);
+			message = new ResponseExecuteError();
 		} else {
+			accountSessions.remove(sessionId);
 			accounts.remove(name);
-			entityManager.deleteSync(account);
 			
-			pt.put(IOUtil.TRUE);
+			message = new ResponseAccountLogout();
 		}
-		send(session, pt);
+		SenderUtils.send(requestAccountLogout.getSender(), message, log);
 	}
-	
-	public void addImoney(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString();
-		int imoney = packet.getInt();
-		Account account = findAccount(name, password);
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
-		if (account == null) {
-			pt.put(IOUtil.FALSE);
-		} else {
-			int result = account.getImoney() + imoney;
-			if (result < 0) {
-				pt.put(IOUtil.FALSE);
-			} else {
-				account.setImoney(result);
-				entityManager.updateSync(account);
-				
-				pt.put(IOUtil.TRUE);
-				pt.putInt(account.getImoney());
-			}
-		}
-		send(session, pt);
-	}
-	
-	public void accountLogout(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString();
+
+	@Subscribe
+	public void changePassword(RequestAccountChangePassword requestAccountChangePassword) {
+//		int serial = packet.getInt();
+		String name = requestAccountChangePassword.getAccount(), password = requestAccountChangePassword.getOldPassword(), newPassword = requestAccountChangePassword.getNewPassword();
 		Account account = findOnlineAccount(name, password);
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
+		IMessage message = new ResponseAccountRegister();
 		if (account == null) {
-			pt.put(IOUtil.FALSE);
-		} else {
-			accounts.remove(name);
-			
-			pt.put(IOUtil.TRUE);
-			pt.putUTF(name);
-		}
-		send(session, pt);
-	}
-	
-	public void changePassword(IoSession session, Packet packet, int returnCode) {
-		int serial = packet.getInt();
-		String name = packet.getString(), password = packet.getString(), newPassword = packet.getString();
-		Account account = findOnlineAccount(name, password);
-		Packet pt = new Packet(returnCode);
-		pt.putInt(serial);
-		if (account == null) {
-			pt.put(IOUtil.FALSE);
+			message = new ResponseExecuteError();
 		} else {
 			account.setPassword(newPassword);
 			entityManager.updateSync(account);
 			
-			pt.put(IOUtil.TRUE);
+			message = new ResponseAccountChangePassword();
 		}
-		send(session, pt);
+		SenderUtils.send(requestAccountChangePassword.getSender(), message, log);
 	}
 	
 	private boolean hasAccountName(String name) {
@@ -227,10 +259,6 @@ public class AuthenticateService {
 	private Account findAccount(String name, String password) {
 		Account account = findOnlineAccount(name, password);
 		return account == null ? entityManager.fetch(Account.class, "from Account where name=? and password=?", name, password) : account;
-	}
-	
-	private static void send(IoSession session, Packet packet) {
-		session.write(new DispatchPacket(DISPATCH_PACKET_ID, packet));
 	}
 
 }
